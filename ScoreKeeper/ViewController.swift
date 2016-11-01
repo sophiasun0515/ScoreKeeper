6a303aefdff730e66dafcaa3d7f8a0e42f91e9f9
sophiasun0515 <ruixuansun@gatech.edu>
2016-11-01 18:34:53 -0400

    Initial Commit

---
+++ ScoreKeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScoreKeeper
+//
+//  Created by Apple on 11/1/16.
+//  Copyright © 2016 Sophia's Secret Garden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
